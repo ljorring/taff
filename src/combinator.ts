1bbd9151228dc2c7bb12a2a2998121a00cea5ad0
Martin Ljorring <ljoerring@hotmail.com>
2020-10-29 17:19:20 +0100

    Update..

--- src/combinator.ts
+++ src/combinator.ts
@@ -16,13 +16,12 @@ let combine: Combine = <I1, U1, NI1, NO1, NI2, NO2>
     (
         m1: IMiddlewareFunction<I1, U1, NI1, NO1>,
         m2: IMiddlewareFunction<NI1, NO1, NI2, NO2>
-    ) => {
-    return async (context: I1, next: (input: NI2) => Promise<NO2>) => {
-
-        let nextResult = await m1(context, async (input: NI1) => {
-            let per = await m2(input, next)
-            return per
-        })
-        return nextResult
-    }
-}
+    ) => 
+    (
+        context: I1,
+        next: (input: NI2) => Promise<NO2>
+    ) =>
+        m1(
+            context,
+            (input: NI1) => m2(input, next)
+        )
