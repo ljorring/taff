cd5ab3225daf8ec51c5cea844fc6651fb168b27a
Martin Ljorring <ljoerring@hotmail.com>
2020-11-02 22:25:51 +0100

    Update..

--- src/combinatorsFunctional.ts
+++ src/combinatorsFunctional.ts
@@ -1,18 +1,34 @@
+import { Context } from "@azure/functions"
+
 declare type ContextAction = (context: Context) => Promise<void>
 
-declare interface MiddleWare<NextT> {
-    (next: (input: NextT) => Promise<ContextAction>): Promise<ContextAction>
+declare interface IMiddleware<NextInput> {
+    (next: (input: NextInput) => Promise<ContextAction>): Promise<ContextAction>
 }
 
-let combine = <TInput1, TInput2>(
-        m1: MiddleWare<TInput1>,
-        m2: MiddleWare<TInput2>
-    ): MiddleWare<[TInput1, TInput2]> =>
+declare interface Middleware<NextInput> extends IMiddleware<NextInput> {
+    bind: <NextInput2>(m2Factory: (input: NextInput) => IMiddleware<NextInput2>)
+}
+
+type MiddlewareFactory<Input, NextInput> = (a: Input) => IMiddleware<NextInput>
+
+let lift = <NextInput>(m: IMiddleware<NextInput>) => {
+    let result: Middleware<NextInput> = m
+
+    result.bind = <NextInput2>(m2Factory: (a: NextInput) => IMiddleware<NextInput2>) => bind(m, m2Factory)
+
+    return result
+}
+
+let combine = <NextInput1, NextInput2>(
+        m1: IMiddleware<NextInput1>,
+        m2: IMiddleware<NextInput2>
+    ): IMiddleware<[NextInput1, NextInput2]> =>
         next => m1(input1 => m2(input2 => next([input1,input2])))
 
-let bind = <TInput1, TInput2>(
-        m1: MiddleWare<TInput1>,
-        m2Factory: (a: TInput1) => MiddleWare<TInput2>
-    ): MiddleWare<TInput2> =>
+let bind = <NextInput1, NextInput2>(
+        m1: IMiddleware<NextInput1>,
+        m2Factory: (a: NextInput1) => IMiddleware<NextInput2>
+    ): IMiddleware<NextInput2> =>
         next => m1(input2 => m2Factory(input2)(next))
 
